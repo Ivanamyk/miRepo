b2237036072ac9d35f7367cf0f2f32cc9cf93dd1
Ivana Yegros Kubicka <ivanamyk@gmail.com>
2020-12-10 22:08:31 -0300

    clase 84

---
+++ ADA/JavaScript/ejercicios en clase/clase-84/enClase.js
@@ -0,0 +1,31 @@
+//FETCH (trae datos de data base)
+const url = "http://url-de-la-api.com";
+fetch(url + '/users')
+    .then(response => {
+        return response.json()
+    }).then(data => {
+        console.log(data);
+    });
+
+//POST (guarda/añade datos)
+const url = "http://url-de-la-api.com";
+const tag = {
+    name: 'tag de prueba',
+};
+//JSON.stringify (pasa datos JSON a strings)
+let tagJsonString = JSON.stringify({ tag: tag });
+//tiene 2 parametros
+fetch(urlBase + '/users', {
+    method: 'POST',
+    Headers: {
+        'Content-Type': 'Application/json'
+    },
+    body: tagJsonString,
+}).then((response => {
+    return response.json()
+})).then(data => {
+    console.log(data);
+})
+
+// //JSON.parse (When receiving data from a web server, the data is always a string. Parse the data with JSON.parse(), 
+//and the data becomes a JavaScript object)
