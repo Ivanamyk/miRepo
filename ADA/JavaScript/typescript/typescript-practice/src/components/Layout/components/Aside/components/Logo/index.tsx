a4c12e1b9c0af614a49ddf93539ee61791bdc7b2
Ivana Yegros Kubicka <ivanamyk@gmail.com>
2021-03-17 00:23:20 -0300

    typescript practica

---
+++ ADA/JavaScript/typescript/typescript-practice/src/components/Layout/components/Aside/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Logo = () => {
+    return (
+        <a href="/" className="logo d-flex justify-content-center mb-4">
+            <img alt="ADA admin logo" height="30" />
+        </a>
+    );
+};
+
+export { Logo };
---
+++ ADA/JavaScript/typescript/typescript-practice/src/components/Layout/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import { NavBar } from "./components";
+import "./styles.css";
+
+const Header: FC = () => {
+  return (
+    <header className="header" >
+      <NavBar />
+    </header>
+  );
+};
+
+export { Header };
+
